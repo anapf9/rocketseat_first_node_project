23ee0b0acfc5e5320db5598944f847334cd167df
Ana <ana.eng.dev@gmail.com>
2020-09-02 18:49:44 -0300

    update avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,12 +3,13 @@ import path from 'path'
 import { request } from 'express'
 import crypto from 'crypto'
 // o crypto é um serviço do node
-
+const tempFolder = path.resolve(__dirname, '..', '..', 'temp')
 export default {
+  directory: tempFolder,
   // neste momento vamos gravar as imagens dentro da aplicação
   // Poré, deve ser usado serviços externos de upload de imagem
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp'),
+    destination: tempFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex')
       const fileName = `${fileHash}-${file.originalname}`
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express'
 import multer from 'multer'
 import uploadConfig from './../config/upload'
 
+import UpdateUserAvatarService from './../services/UpdateUserAvatarService'
 import CreateUserService from './../services/CreateUserService'
-
 import ensureAuthenticated from './../middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
@@ -28,8 +28,18 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  console.log(request.file)
-  return response.json({ ok: true })
+  // console.log(request.file)
+  try {
+    const updateUserAvatar = new UpdateUserAvatarService
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename
+    })
+    delete user.password
+    return response.json(user)
+  } catch (error) {
+    return response.status(400).json({ error: error.message })
+  }
 })
 
 export default usersRouter
