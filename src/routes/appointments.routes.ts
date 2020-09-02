3f137e2e9705bd9e249597f6880df6870af64644
Ana <ana.eng.dev@gmail.com>
2020-09-02 11:50:41 -0300

    refratoring provider_id in Service and Repository, create users with relation appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router, response } from 'express'
+import { Router } from 'express'
 import { parseISO } from 'date-fns'
 import { getCustomRepository } from 'typeorm'
 
@@ -16,14 +16,14 @@ appointmentsRouter.get('/', async (req, res) => {
 
 appointmentsRouter.post('/', async (req, res) => {
   try {
-    const { provider, date } = req.body
+    const { provider_id, date } = req.body
 
     // const parsedDate = startOfHour(parseISO(date))
     const parsedDate = parseISO(date)
 
     const createAppointmentService = new CreateAppointmentService()
 
-    const appointment = await createAppointmentService.execute({ date: parsedDate, provider })
+    const appointment = await createAppointmentService.execute({ date: parsedDate, provider_id })
 
     return res.json(appointment)
   } catch (err) {
